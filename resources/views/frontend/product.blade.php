cc596fd1167136c828739ea1b1bd470cc6d39405
fawait626 <fawait626gmail.com>
2023-04-06 10:10:54 +0700

    update terbaru

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -30,8 +30,6 @@
                         <br>
                         <div class="card_area d-flex align-items-center">
                             <a class="primary-btn" href="#">Add to Cart</a>
-                            <a class="icon_btn" href="#"><i class="lnr lnr lnr-diamond"></i></a>
-                            <a class="icon_btn" href="#"><i class="lnr lnr lnr-heart"></i></a>
                         </div>
                     </div>
                 </div>
